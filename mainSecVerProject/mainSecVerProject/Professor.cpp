bc874d0e4ad2314236f2f5d92f1f4a0838189c8f
luismorenoz <luis.moreno.zuniga@est.una.ac.cr>
2025-02-10 15:56:59 -0800

    Methods CRUD from Professor.cpp were made

--- mainSecVerProject/mainSecVerProject/Professor.cpp
+++ mainSecVerProject/mainSecVerProject/Professor.cpp
@@ -30,4 +30,65 @@ string Professor::getDegree() {
     return degree;
 }
 
+void addProfessor(ProfessorNode*& head, string id, string name, string degree) {
+    Professor newProfessor(id, name, degree);
+    ProfessorNode* newNode = new ProfessorNode(newProfessor);
+
+    if (!head) {
+        head = newNode;
+    } else {
+        ProfessorNode* temp = head;
+        while (temp->next) temp = temp->next;
+        temp->next = newNode;
+    }
+}
+
+void viewProfessorDetails(ProfessorNode* head, string id) {
+    ProfessorNode* temp = head;
+    while (temp) {
+        if (temp->data.id == id) {
+            cout << "Profesor: " << temp->data.name << ", ID: " << temp->data.id << endl;
+            return;
+        }
+        temp = temp->next;
+    }
+    cout << "Profesor no encontrado." << endl;
+}
+
+void updateProfessorDetails(ProfessorNode* head, string id, string newName, string newDegree) {
+    ProfessorNode* temp = head;
+    while (temp) {
+        if (temp->data.id == id) {
+            temp->data.setName(newName);
+            temp->data.setDegree(newDegree);
+            cout << "Profesor actualizado." << endl;
+            return;
+        }
+        temp = temp->next;
+    }
+    cout << "Profesor no encontrado." << endl;
+}
+
+void removeProfessor(ProfessorNode*& head, string id) {
+    if (!head) return;
+    if (head->data.id == id) {
+        ProfessorNode* temp = head;
+        head = head->next;
+        delete temp;
+        cout << "Profesor eliminado." << endl;
+        return;
+    }
+    ProfessorNode* temp = head;
+    while (temp->next && temp->next->data.id != id) {
+        temp = temp->next;
+    }
+    if (temp->next) {
+        ProfessorNode* toDelete = temp->next;
+        temp->next = temp->next->next;
+        delete toDelete;
+        cout << "Profesor eliminado." << endl;
+    } else {
+        cout << "Profesor no encontrado." << endl;
+    }
+}
 
