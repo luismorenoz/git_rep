e5d641b4606ba0c22f83ec40ab8afb47e4533d20
Barrantes-0810 <joshua.barrantes.granados@est.una.ac.cr>
2025-02-10 17:43:17 -0600

    Update ScheduleTime.h

--- mainSecVerProject/mainSecVerProject/ScheduleTime.h
+++ mainSecVerProject/mainSecVerProject/ScheduleTime.h
@@ -1,5 +1,25 @@
 #pragma once
-class ScheduleTime
-{
-};
+#include <string>
+#include "Schedule.h"
+using namespace std;
 
+class ScheduleTime {
+private:
+    Schedule scheduleCode;
+    string day;
+    int startHour;
+    int finishHour;
+
+public:
+    ScheduleTime();
+    ScheduleTime(Schedule scheduleCode, string day, int startHour, int finishHour);
+
+    void setScheduleCode(Schedule scheduleCode);
+    void setDay(string day);
+    void setStartHour(int startHour);
+    void setFinishHour(int finishHour);
+    Schedule getScheduleCode();
+    string getDay();
+    int getStartHour();
+    int getFinishHour();
+};
