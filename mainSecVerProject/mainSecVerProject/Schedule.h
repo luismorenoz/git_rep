df7cc7562bc5b5a5061374893a84d10aed51a9ec
Barrantes-0810 <joshua.barrantes.granados@est.una.ac.cr>
2025-02-10 17:34:58 -0600

    Update Schedule.h

--- mainSecVerProject/mainSecVerProject/Schedule.h
+++ mainSecVerProject/mainSecVerProject/Schedule.h
@@ -1,5 +1,21 @@
 #pragma once
-class Schedule
-{
-};
+#include <string>
+using namespace std;
 
+class Schedule {
+private:
+    string code;
+    int classroom;
+    string classCode;
+
+public:
+    Schedule();
+    Schedule(string code, int classroom, string classCode);
+
+    void setCode(string code);
+    void setClassroom(int classroom);
+    void setClassCode(string classCode);
+    string getCode();
+    int getClassroom();
+    string getClassCode();
+};
