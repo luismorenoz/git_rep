f6c475f81f660db97c4954d49c5a5a3b9a6ad014
Barrantes-0810 <joshua.barrantes.granados@est.una.ac.cr>
2025-02-10 17:27:23 -0600

    Update Professor.h

--- mainSecVerProject/mainSecVerProject/Professor.h
+++ mainSecVerProject/mainSecVerProject/Professor.h
@@ -1,5 +1,24 @@
 #pragma once
 class Professor
-{
+#include <string>
+using namespace std;
+
+class Professor {
+private:
+    string id;
+    string name;
+    string degree;
+
+public:
+    Professor();
+    Professor(string id, string name, string degree);
+
+    void setId(string id);
+    void setName(string name);
+    void setDegree(string degree);
+    string getId();
+    string getName();
+    string getDegree();
 };
+
 
