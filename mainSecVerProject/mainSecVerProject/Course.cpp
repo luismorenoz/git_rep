6df4ddc089e2bceb55fe6bba7e1fa13f87aa6628
Barrantes-0810 <joshua.barrantes.granados@est.una.ac.cr>
2025-02-10 17:37:59 -0600

    Update Course.cpp

--- mainSecVerProject/mainSecVerProject/Course.cpp
+++ mainSecVerProject/mainSecVerProject/Course.cpp
@@ -8,7 +8,6 @@ Course::Course() {
     level = 0;
     condition = " ";
 }
-
 Course::Course(string code, string name, string major, int credits, int level, string condition) {
     this->code = code;
     this->name = name;
@@ -17,51 +16,39 @@ Course::Course(string code, string name, string major, int credits, int level, s
     this->level = level;
     this->condition = condition;
 }
-
 void Course::setCode(string code) {
     this->code = code;
 }
-
 void Course::setName(string name) {
     this->name = name;
 }
-
 void Course::setMajor(string major) {
     this->major = major;
 }
-
 void Course::setCredits(int credits) {
     this->credits = credits;
 }
-
 void Course::setLevel(int level) {
     this->level = level;
 }
-
 void Course::setCondition(string condition) {
     this->condition = condition;
 }
-
 string Course::getCode() {
     return code;
 }
-
 string Course::getName() {
     return name;
 }
-
 string Course::getMajor() {
     return major;
 }
-
 int Course::getCredits() {
     return credits;
 }
-
 int Course::getLevel() {
     return level;
 }
-
 string Course::getCondition() {
     return condition;
 }
