a8f198236396a7384406acfce8cadb6dc2349e01
Barrantes-0810 <joshua.barrantes.granados@est.una.ac.cr>
2025-02-10 17:36:39 -0600

    Update Schedule.cpp

--- mainSecVerProject/mainSecVerProject/Schedule.cpp
+++ mainSecVerProject/mainSecVerProject/Schedule.cpp
@@ -1,1 +1,30 @@
 #include "Schedule.h"
+
+Schedule::Schedule() {
+    code = " ";
+    classroom = 0;
+    classCode = " ";
+}
+Schedule::Schedule(string code, int classroom, string classCode) {
+    this->code = code;
+    this->classroom = classroom;
+    this->classCode = classCode;
+}
+void Schedule::setCode(string code) {
+    this->code = code;
+}
+void Schedule::setClassroom(int classroom) {
+    this->classroom = classroom;
+}
+void Schedule::setClassCode(string classCode) {
+    this->classCode = classCode;
+}
+string Schedule::getCode() {
+    return code;
+}
+int Schedule::getClassroom() {
+    return classroom;
+}
+string Schedule::getClassCode() {
+    return classCode;
+}
