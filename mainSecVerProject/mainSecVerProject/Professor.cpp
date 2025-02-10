4ee566390fbd22a49e9722c7adf6ee062c8ef527
Barrantes-0810 <joshua.barrantes.granados@est.una.ac.cr>
2025-02-10 17:30:53 -0600

    Update Professor.cpp

--- mainSecVerProject/mainSecVerProject/Professor.cpp
+++ mainSecVerProject/mainSecVerProject/Professor.cpp
@@ -1,1 +1,33 @@
 #include "Professor.h"
+
+Professor::Professor() {
+    id = "";
+    name = "";
+    degree = "";
+}
+
+Professor::Professor(string id, string name, string degree) {
+    this->id = id;
+    this->name = name;
+    this->degree = degree;
+}
+void Professor::setId(string id) {
+    this->id = id;
+}
+void Professor::setName(string name) {
+    this->name = name;
+}
+void Professor::setDegree(string degree) {
+    this->degree = degree;
+}
+string Professor::getId() {
+    return id;
+}
+string Professor::getName() {
+    return name;
+}
+string Professor::getDegree() {
+    return degree;
+}
+
+
