67911fa52c5ffc2e87bf98054b26571c099862a9
Barrantes-0810 <joshua.barrantes.granados@est.una.ac.cr>
2025-02-10 18:07:35 -0600

    Update Condition.cpp

--- mainSecVerProject/mainSecVerProject/Condition.cpp
+++ mainSecVerProject/mainSecVerProject/Condition.cpp
@@ -1,1 +1,22 @@
 #include "Condition.h"
+
+Condition::Condition() {
+    courseA = "";
+    courseB = "";
+}
+Condition::Condition(string courseA, string courseB) {
+    this->courseA = courseA;
+    this->courseB = courseB;
+}
+void Condition::setCourseA(string courseA) {
+    this->courseA = courseA;
+}
+void Condition::setCourseB(string courseB) {
+    this->courseB = courseB;
+}
+string Condition::getCourseA() {
+    return courseA;
+}
+string Condition::getCourseB() {
+    return courseB;
+}
