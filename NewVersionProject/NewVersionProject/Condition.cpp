e0a54ea2b84055e976892af811b4360bb7cffa03
Barrantes-0810 <joshua.barrantes.granados@est.una.ac.cr>
2025-02-10 21:25:45 -0600

    Update Condition.cpp

--- NewVersionProject/NewVersionProject/Condition.cpp
+++ NewVersionProject/NewVersionProject/Condition.cpp
@@ -1,37 +1,26 @@
 #include "Condition.h"
-
 using namespace std;
 
-// Constructor vacío
 Condition::Condition() {
-    courseA = Registration();  // Llamada al constructor vacío de Registration
-    courseB = Registration();  // Llamada al constructor vacío de Registration
+    courseA = Registration();
+    courseB = Registration();
 }
-
-// Constructor con parámetros
 Condition::Condition(Registration courseA, Registration courseB) {
     this->courseA = courseA;
     this->courseB = courseB;
 }
-
-// Getters y Setters
-
 Registration Condition::getCourseA() const {
     return courseA;
 }
-
 void Condition::setCourseA(const Registration& courseA) {
     this->courseA = courseA;
 }
-
 Registration Condition::getCourseB() const {
     return courseB;
 }
-
 void Condition::setCourseB(const Registration& courseB) {
     this->courseB = courseB;
 }
-
 void checkConflict() {
 
 
