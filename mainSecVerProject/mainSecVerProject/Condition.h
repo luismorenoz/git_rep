f25c516d20b73bcdee9a0c7c772dc2dc3123842c
Barrantes-0810 <joshua.barrantes.granados@est.una.ac.cr>
2025-02-10 18:06:29 -0600

    Update Condition.h

--- mainSecVerProject/mainSecVerProject/Condition.h
+++ mainSecVerProject/mainSecVerProject/Condition.h
@@ -1,5 +1,18 @@
 #pragma once
-class Condition
-{
-};
+#include <string>
+using namespace std;
 
+class Condition {
+private:
+    string courseA;
+    string courseB;
+
+public:
+    Condition();
+    Condition(string courseA, string courseB);
+
+    void setCourseA(string courseA);
+    void setCourseB(string courseB);
+    string getCourseA();
+    string getCourseB();
+};
