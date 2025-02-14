3aa4a6e4880028991223c5d2762cc30f891a7de2
MaFerJimenez-14 <maria.jimenez.alpizar@est.una.ac.cr>
2025-02-13 18:34:48 -0600

    Update Condition.h adding conditions to the condition.h class

--- mainSecVerProject/mainSecVerProject/Condition.h
+++ mainSecVerProject/mainSecVerProject/Condition.h
@@ -4,15 +4,25 @@ using namespace std;
 
 class Condition {
 private:
-    string courseToRegister;
-    string courseCondition;
+    string courseToRegister;               // Curso que se quiere matricular
+    vector<string> prerequisites;          // Cursos requisitos
 
 public:
+    // Constructores
     Condition();
-    Condition(string courseA, string courseB);
+    Condition(string courseToRegister, vector<string> prerequisites);
 
-    void courseToRegister(string courseToRegister);
-    void courseCondition(string courseB);
-    string courseToRegister();
-    string courseCondition();
+    // Setters
+    void setCourseToRegister(string courseToRegister);
+    void setPrerequisites(vector<string> prerequisites);
+
+    // Getters
+    string getCourseToRegister();
+    vector<string> getPrerequisites();
+
+    // Verifica si el estudiante ha aprobado todos los cursos requisitos
+    bool canRegister(const vector<string>& passedCourses) const;
+
+    // Método estático para definir las reglas de prerequisitos
+    static unordered_map<string, vector<string>> getDefaultPrerequisites();
 };
